7424186be2a968882c07c47d4d125a256004eba6
Bill <billvaleriote@gmail.com>
2013-02-10 02:14:05 -0500

    Added intersectsWith to Line, only works for other Lines at the moment.

--- js/Line.js
+++ js/Line.js
@@ -16,6 +16,10 @@ Line.prototype.getPt2 = function () {
   return this.pt2 
 }
 
+Line.prototype.slope = function() {
+  return (this.getPt1().getY() - this.getPt2().getY()) / (this.getPt1().getX() - this.getPt2().getX());
+}
+
 Line.prototype.isSame = function (Ln) {
   if (this.pt1.isSame(Ln.getPt1()) && this.pt2.isSame(Ln.getPt2())) return true;
   else if (this.pt1.isSame(Ln.getPt2()) && this.pt2.isSame(Ln.getPt1())) return true;
@@ -43,5 +47,50 @@ Line.prototype.draw = function (colour) {
   ctx.lineTo(pt2X, pt2Y);
   ctx.closePath();
   ctx.stroke();
+}
+
+
+Line.prototype.intersectsWith = function(shape){
+  var pointsOfIntersection = [];
+
+  if (shape instanceof Circle){
+    console.log("Not doin' circles yet!");
+  }
+  else if (shape instanceof Line){
+   var m1 = this.slope();
+   var m2 = shape.slope();
+   var x;
+   var y;
+
+   var b1 = this.getPt1().getY() - this.getPt1().getX()*m1;
+   var b2 = shape.getPt1().getY() - this.getPt1().getX()*m2;
+      
+
+   if (m1 != m2){
+    if (m1 == Infinity){
+      x = this.getPt1().getX();
+      y = m2 * x + b2;
+    }
+    else if (m2 == Infinity){
+      x = shape.getPt1().getX();
+      y = m1 * x + b1;
+    }
+    else{
+      x = (b2 - b1) / (m1 - m2);
+      y = m1 * x + b1;
+    }
+
+    if ((this.getPt1().getX() <= x && x <= this.getPt2().getX()) ||
+         this.getPt2().getX() <= x && x <= this.getPt1().getX()){
+           pointsOfIntersection.push(new Point(this.getCtx(), x, y));
+    }
+   }
+  }
+  else{
+    console.log("Not a shape!");
+  }
+  
+  return pointsOfIntersection;
+
 }
 
