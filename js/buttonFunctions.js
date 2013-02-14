10f4cb1dd8ca913036f96f1e5390715f1da98bbe
colinrdavidson <colinrdavidson@gmail.com>
2013-02-13 22:49:32 -0500

    Moved functions involving buttons from index to here.

---
+++ js/buttonFunctions.js
@@ -0,0 +1,58 @@
+function clickStart(pointArray){
+  for(var i = 0; i < pointArray.length; i++){
+    pointArray[i].draw();
+  }
+} 
+
+function clickLine(ctx, currentPoint1, currentPoint2, lineArray, intersectionPoints){
+  if (currentPoint1 && currentPoint2)
+  {
+    var line = new Line(ctx, currentPoint1, currentPoint2);
+
+    var contains = false;
+
+    for (var i = 0; i < lineArray.length; i++){
+      if (line.isSame(lineArray[i])){
+        contains = true;
+        break;
+      }
+    }
+
+    if (!contains){
+
+      for(var i = 0; i < lineArray.length; i++){
+        var intersections = line.intersectsWith(lineArray[i]);
+
+        intersections[0] && intersections[0].draw();
+      }
+
+      lineArray.push(line);
+    }
+
+    console.log(line.toString());
+    line.draw();
+  }
+}
+
+function clickCircle(ctx, currentPoint1, currentPoint2, circleArray){
+  if (currentPoint1 && currentPoint2)
+  {
+    var circle = new Circle(ctx, currentPoint1, currentPoint2);
+
+    var contains = false;
+
+    for (var i = 0; i < circleArray.length; i++){
+      if (circle.isSame(circleArray[i])){
+        contains = true;
+        break;
+      }
+    }
+
+    if (!contains){
+      circleArray.push(circle);
+    }
+
+    console.log(circle.toString());
+    circle.draw();
+  }
+}
