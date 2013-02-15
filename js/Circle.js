88c0a40968130c97f8754e1a8343735be1669a00
colinrdavidson <colinrdavidson@gmail.com>
2013-02-15 15:22:23 -0500

    Implemented Circle.intersectsWith.

--- js/Circle.js
+++ js/Circle.js
@@ -56,6 +56,91 @@ Circle.prototype.draw = function(colour) {
   ctx.stroke();
 }
 
+Circle.prototype.intersectsWith = function(shape){
+  var pointsOfIntersection = [];
+
+  if (shape instanceof Circle){
+    var x0 = this.getFoc().getX();
+    var y0 = this.getFoc().getY();
+    var r0 = pointPointDistance(this.getFoc().getX(), this.getFoc().getY(), this.getLoc().getX(), this.getLoc().getY());
+    var x1 = shape.getFoc().getX(); 
+    var y1 = shape.getFoc().getY();
+    var r1 = pointPointDistance(shape.getFoc().getX(), shape.getFoc().getY(), shape.getLoc().getX(), shape.getLoc().getY());
+    var a, dx, dy, d, h, rx, ry;
+    var x2, y2;
+
+    /* dx and dy are the vertical and horizontal distances between
+     * the circle centers.
+     */
+    dx = x1 - x0;
+    dy = y1 - y0;
+
+    /* Determine the straight-line distance between the centers. */
+    d = Math.sqrt((dy*dy) + (dx*dx));
+
+    /* Check for solvability. */
+    if (d > (r0 + r1)) {
+      /* no solution. circles do not intersect. */
+      console.log("No intersection, beside each other");
+    }
+    else if (d < Math.abs(r0 - r1)) {
+      /* no solution. one circle is contained in the other */
+      console.log("No intersection, one inside other");
+    }
+    else {
+
+      /* 'point 2' is the point where the line through the circle
+       * intersection points crosses the line between the circle
+       * centers.  
+       */
+
+      /* Determine the distance from point 0 to point 2. */
+      a = ((r0*r0) - (r1*r1) + (d*d)) / (2.0 * d) ;
+
+      /* Determine the coordinates of point 2. */
+      x2 = x0 + (dx * a/d);
+      y2 = y0 + (dy * a/d);
+
+      /* Determine the distance from point 2 to either of the
+       * intersection points.
+       */
+      h = Math.sqrt((r0*r0) - (a*a));
+
+      /* Now determine the offsets of the intersection points from
+       * point 2.
+       */
+      rx = -dy * (h/d);
+      ry = dx * (h/d);
+
+      /* Determine the absolute intersection points. */
+      var xi = x2 + rx;
+      var xi_prime = x2 - rx;
+      var yi = y2 + ry;
+      var yi_prime = y2 - ry;
+
+      var point1 = new Point(this.ctx, xi, yi);
+      var point2 = new Point(this.ctx, xi_prime, yi_prime);
+
+      if (point1.isSame(point2)){
+        pointsOfIntersection.push(point1);
+      }
+      else {
+        pointsOfIntersection.push(point1);
+        pointsOfIntersection.push(point2);
+      }
+    }
+  }
+
+  else if (shape instanceof Line){
+    console.log("Not tongiht byes");
+  }
+  else{
+    console.log("Not a shape");
+  }
+
+  return pointsOfIntersection;
+}
+
 Circle.prototype.toString = function() {
   return "Circle: (" + this.getFoc().toString() + ", " + this.getLoc().toString() + ")";
 }
