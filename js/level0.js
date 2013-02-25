ed56c686811ef0b120c049efceb35f3f85f953a9
colinrdavidson <colinrdavidson@gmail.com>
2013-02-25 14:28:52 -0500

    A very simple json level.

---
+++ js/level0.js
@@ -0,0 +1,27 @@
+{
+  name: "Level0",
+  points:
+  {
+    0: [100, 200],
+    1: [300, 200]
+  },
+  lines:
+  {
+  },
+  circles:
+  {
+  },
+  goals:
+  {
+    points:
+    {
+      0: [200, 200]
+    },
+    lines:
+    {
+    },
+    circles:
+    {
+    }
+  }
+}
