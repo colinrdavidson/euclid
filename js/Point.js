c76b829b232a92bce818860dc8f146904659ab97
colinrdavidson <colinrdavidson@gmail.com>
2013-02-25 22:45:58 -0500

    Fixed some spacing.

--- js/Point.js
+++ js/Point.js
@@ -3,19 +3,19 @@ var Point = function(layer, x, y){
   this.x = Math.round(x*10000000)/10000000;
   this.y = Math.round(y*10000000)/10000000;
 }
-Point.prototype.Layer = function() {
+Point.prototype.Layer = function () {
   return this.layer; 
 }
 
-Point.prototype.X = function() {
+Point.prototype.X = function () {
   return this.x; 
 }
 
-Point.prototype.Y = function() {
+Point.prototype.Y = function () {
   return this.y; 
 }
 
-Point.prototype.copy = function(layer) {
+Point.prototype.copy = function (layer) {
   if (!layer){
     return new Point(this.layer, this.x, this.y);
   }
@@ -23,18 +23,18 @@ Point.prototype.copy = function(layer) {
     return new Point(layer, this.x, this.y);
 }
 
-Point.prototype.isSame = function(pt) {
+Point.prototype.isSame = function (pt) {
   if ((pt instanceof Point) && (this.x == pt.X() && this.y == pt.Y()))
     return true;
   else
     return false; 
 }
 
-Point.prototype.toString = function(){
+Point.prototype.toString = function (){
   return "Point: (" + this.X() + ", " + this.Y() + ")";
 }
 
-Point.prototype.isInArray = function(array){
+Point.prototype.isInArray = function (array){
   //assume this array is full of points
   for(var i = 0; i < array.length; i++){
     if (this.isSame(array[i]))
