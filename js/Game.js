b786d72c5eef55ed70e9bc0c3418210e56b6477e
Bill <billvaleriote@gmail.com>
2013-02-25 23:03:26 -0500

    Basic level parsing, still need to find intersections and deal with goal points.

--- js/Game.js
+++ js/Game.js
@@ -1,17 +1,20 @@
-var Game = function (drawer) {
+var Game = function () {
   //assuming 
  // this.drawer = drawer;
   //this.state = new State();
   // State
+  //currentlevel
 }
 
 Game.prototype.loadLevel = function (levelName) {
 
   //find levelName in levelArray
   var level = Levels[levelName]; 
+
+  this.currentLevel = level;
   
   if (level){
-   //this.state = LevelParse(level);
+   this.state = LevelParse(level);
    //this.drawer.Draw(this.state);
   }
   else{
@@ -19,6 +22,49 @@ Game.prototype.loadLevel = function (levelName) {
   }
 }
 
-levelParse = function (level) {
+LevelParse = function (level) {
   // turns this object into a state
+  var state = new State();
+
+  var points = level.points;
+  if (points){
+    for (var i = 0; i < ObjectCount(points); i++){
+      state.addPoint(new Point(points[i].x, points[i].y));
+    }
+  }
+  var lines = level.lines;
+
+  if (lines){
+    for (var i = 0; i < ObjectCount(lines); i++){
+      var point1 = state.Points()[lines[i].pt1];
+      var point2 = state.Points()[lines[i].pt2];
+
+      if (point1 && point2){
+        state.addLine(new Line(point1, point2));  
+      }
+      else{
+        console.log("those points don't exist");
+      }
+    }
+  }
+
+  var circles = level.circles;
+  if (circles){
+    for (var i = 0; i < ObjectCount(circles); i++){
+      var foc = state.Points()[circles[i].foc];
+      var loc = state.Points()[circles[i].loc];
+
+      if (point1 && point2){
+        state.addLine(new Circle(foc, loc));  
+      }
+      else{
+        console.log("those points don't exist");
+      }
+    }
+  }
+
+  //need to do the same for goals
+
+  return state;
+
 }
