e8db559354e612482ff8257541915e64a46859d9
colinrdavidson <colinrdavidson@gmail.com>
2013-02-27 03:58:50 -0500

    Huge changes to how objects are added to the state.

--- js/State.js
+++ js/State.js
@@ -5,38 +5,70 @@ var State = function () {
   this.circles = [];
 }
 
-State.prototype.addCircle = function (circle) {
-  if (circle instanceof Circle){
-    this.circles.push(circle);
-  }
-  else {
-    console.log("not a circle.");
-  }
-}
-
 State.prototype.addPoint = function (point) {
   if (point instanceof Point){
-    this.points.push(point);
+    if (!point.isInArray(this.points)){
+      this.points.push(point);
+      console.log(point.toString());
+    }
   }
   else {
-    console.log("not a point.");
-  }
-}
-
-State.prototype.addPotentialPoint = function (point) {
-  if (point instanceof Point){
-    this.potentialPoints.push(point);
-  }
-  else {
-    console.log("not a point.");
+    console.log("Point already in array.");
   }
 }
 
 State.prototype.addLine = function (line) {
   if (line instanceof Line){
-    this.lines.push(line);
-  }
-  else {
-    console.log("not a line.");
+    if (!line.isInArray(this.lines)){
+      for (var i = 0; i < this.lines.length; i++){
+        var potentialPoints = newLine.intersectsWith(this.lines[i]);
+
+        if (potentialPoints){
+          for (var j = 0; j < potentialPoints.length; j++){
+            this.addPotentialPoint(potentialPoints[j]);  
+          }
+        }
+      }
+
+      this.lines.push(line);
+      console.log(line.toString());
+    }
+    else {
+      console.log("Line already in array");
+    }
   }
 }
+
+State.prototype.addCircle = function (circle) {
+  if(circle instanceof Circle){
+    if (!circle.isinArray(this.circles){
+      for (var i = 0; i < this.lines.length; i++){
+        var potentialPoints = newCircle.intersectsWith(this.lines[i]);
+
+        if (potentialPoints){
+          for (var j = 0; j < potentialPoints.length; j++){
+            this.addPotentialPoint(potentialPoints[j]);  
+          }
+        }
+      }
+
+      this.circles.push(circle);
+      console.log(circle.toString());
+    } 
+    else{
+      console.log("Circle already in array.");
+    }
+  }
+}
+State.prototype.addPotentialPoint = function (point) {
+  if (point instanceof Point){
+    if (!point.isInArray(this.potentialPoints)){
+      this.potentialPoints.push(point);
+    }
+  }
+  else {
+    console.log("not a point.");
+  }
+}
+
+
