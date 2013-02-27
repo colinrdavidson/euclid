5242d1032abcbfae0956dc9edaf6dd10ab95bed4
colinrdavidson <colinrdavidson@gmail.com>
2013-02-27 04:00:46 -0500

    Big changes to function signatures. Not tested. Much testing to do everywhere.

--- js/init.js
+++ js/init.js
@@ -22,71 +22,59 @@ function init () {
     4 : ctxUser
   };
 
-  drawer = new Drawer();
-  drawer.initialize(contexts);
+  //compute size of canvas
+  var width = canvasUser.width;
+  var height = canvasUser.height;
+  
+  var offsetTop = canvasInitial.offsetTop;
+  var offsetLeft = canvasInitial.offsetLeft;
 
-  game = new Game();
+  drawer = new Drawer();
+  drawer.initialize(contexts, height, width);
+
+  game = new Game(drawer);
   game.loadLevel("Level0");
 
   drawer.draw(game.state);
   drawer.draw(game.state.potentialPoints);
-
-  //All mouse events occur on user layer as it is on top
-
-  //Offset so we can compute location on canvas form location on screen
-  var width = canvasUser.width;
-  var height = canvasUser.height;
-  var offsetTop = canvasInitial.offsetTop;
-  var offsetLeft = canvasInitial.offsetLeft;
-
-  //Arrays
-  var pointArray = [];
-  var lineArray = [];
-  var circleArray = [];
-
-  var intersectionPoints = [];
 
   //Special Points
   var mouseOverPoint = null; 
   var currentPoint1 = null;
   var currentPoint2 = null;
 
-  //test points
-  var point1 = new Point(0, 100, 200);
-  var point2 = new Point(0, 300, 200);
-  pointArray.push(point1, point2);
-
   //Button Events
   $("#startButton").click(function (){
-    clickStart(pointArray);
+    clickStart();
   });
 
   $("#lineButton").click(function (){ 
-    clickLine(1, currentPoint1, currentPoint2, pointArray, lineArray, circleArray, intersectionPoints); 
+    clickLine(1, currentPoint1, currentPoint2); 
   });
 
   $("#circleButton").click(function (){ 
-    clickCircle(1, currentPoint1, currentPoint2, pointArray, lineArray, circleArray, intersectionPoints);
+    clickCircle(1, currentPoint1, currentPoint2);
   });
 
   //Mouse Events
+  //All mouse events occur on user layer as it is on top
   $("#user").mousemove(function (e){ 
-    mouseOverPoint = mouseMove(3, pointArray, e.pageX - offsetLeft, e.pageY - offsetTop);
+    mouseOverPoint = mouseMove(3, e.pageX - offsetLeft, e.pageY - offsetTop);
     drawer.clearLayer(3);
     if (mouseOverPoint){
-      drawer.draw(mouseOverPoint, "#0000FF");
+      game.draw(mouseOverPoint, "#0000FF");
     }
   });
 
   $("#user").mousedown(function (e){
     if (mouseOverPoint){
-      var clickedPoints = clickCanvas(2, pointArray, lineArray, circleArray,  mouseOverPoint, currentPoint1, currentPoint2);
+      var clickedPoints = clickCanvas(2, mouseOverPoint, currentPoint1, currentPoint2);
       currentPoint1 = clickedPoints[0];
       currentPoint2 = clickedPoints[1];
 
-      ctxCurrent.clearRect(0,0,400,400);
-      if (currentPoint1) drawer.draw(currentPoint1, "#00FF00");
-      if (currentPoint2) drawer.draw(currentPoint2, "#FF0000");
+      game.clearLayer(2);
+      if (currentPoint1) game.draw(currentPoint1, "#00FF00");
+      if (currentPoint2) game.draw(currentPoint2, "#FF0000");
     }
   });
 }
