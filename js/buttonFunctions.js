4fc47195e499def7ce15d70bf141393addad7655
colinrdavidson <colinrdavidson@gmail.com>
2013-02-27 03:09:12 -0500

    Button functions now interfacing with game.

--- js/buttonFunctions.js
+++ js/buttonFunctions.js
@@ -1,88 +1,24 @@
-function clickStart(state){
+function clickStart(){
   console.log("Clicked Start.");
-  for(var i = 0; i < pointArray.length; i++){
-    ;
-  }
+  game.draw();
 } 
 
-function clickLine(layer, currentPoint1, currentPoint2, state){
+function clickLine(layer, currentPoint1, currentPoint2){
   console.log("Clicked Line");
   if (currentPoint1 && currentPoint2)
   {
     var line = new Line(layer, currentPoint1, currentPoint2);
-    
-
-    var contains = false;
-
-    for (var i = 0; i < lineArray.length; i++){
-      if (line.isSame(lineArray[i])){
-        contains = true;
-        break;
-      }
-    }
-
-    if (!contains){
-      shapesArray = lineArray.concat(circleArray);
-      for(var i = 0; i < shapesArray.length; i++){
-        var intersections = line.intersectsWith(shapesArray[i]);
-
-        for (var j = 0; j < intersections.length; j++){
-          if (intersections[j]){
-            if (!intersections[j].isInArray(intersectionPoints)){
-              intersectionPoints.push(intersections[j]);
-            }
-
-            if (!intersections[j].isInArray(pointArray)){
-              pointArray.push(intersections[j]);
-              console.log(intersections[j].toString());
-              drawer.draw(intersections[j]);
-            }
-          }
-        }
-      }
-
-      lineArray.push(line);
-      console.log(line.toString());
-      drawer.draw(line);
+    game.add(line);
     }
   }
 }
 
-function clickCircle(layer, currentPoint1, currentPoint2, pointArray, lineArray, circleArray, intersectionPoints){
+function clickCircle(layer, currentPoint1, currentPoint2){
   console.log("Clicked Start");
   if (currentPoint1 && currentPoint2)
   {
     var circle = new Circle(layer, currentPoint1, currentPoint2);
-
-    var contains = false;
-
-    for (var i = 0; i < circleArray.length; i++){
-      if (circle.isSame(circleArray[i])){
-        contains = true;
-        break;
-      }
-    }
-
-    if (!contains){
-      shapesArray = lineArray.concat(circleArray);
-      for(var i = 0; i < shapesArray.length; i++){
-        var intersections = circle.intersectsWith(shapesArray[i]);
-
-        for (var j = 0; j < intersections.length; j++){
-          if (!intersections[j].isInArray(intersectionPoints)){
-            intersectionPoints.push(intersections[j]);
-          }
-
-          if (!intersections[j].isInArray(pointArray)){
-            pointArray.push(intersections[j]);
-            console.log(intersections[j].toString());
-            drawer.draw(intersections[j]);
-          }
-        }
-      }
-      circleArray.push(circle);
-      console.log(circle.toString());
-      drawer.draw(circle);
+    game.add(circle)
     }
   }
 }
